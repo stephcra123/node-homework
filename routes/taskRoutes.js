461e09372421a71bd4a5895caf395a4ae9d8a6e9
stephcra123 <186585465+stephcra123@users.noreply.github.com>
2026-06-11 23:17:11 -0400

    add recycle bin routes and controller functions

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,6 +4,8 @@ const { index, create, show, update, deleteTask, bulkCreate } = require("../cont
 
 router.route("/").get(index).post(create);
 router.route("/bulk").post(bulkCreate);
+router.route("/trash").get(getTrashed).delete(emptyTrash);
+router.route("/trash/:id").patch(restoreTask);
 router.route("/:id").get(show).patch(update).delete(deleteTask);
 
 module.exports = router;
